f2bde8a8d0c16e524d5af6681f517bd97dca3ad2
Atte Suni <sunihuoso@hotmail.com>
2017-12-07 17:41:36 +0200

    Testaus

---
+++ Konsolikoodia/Konsolikoodia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsolikoodia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tiedosto = @"I:\Visual Studio Clones\Konsolikoodia\numero.txt";
+            //tiedoston luku
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu numero on " + aiempiArvo + ".");
+            }
+            else
+            {
+                Console.WriteLine("Jaa.");
+            }
+
+
+            int lämpötila = 0;
+            Console.Write("Aseta numero: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            //tiedostoon kirjoittaminen
+
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Asetit numeron " + int.Parse(syöte) + ".");
+            Console.ReadLine();
+        }
+    }
+}
